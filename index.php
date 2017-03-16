5358f1df58958c2ca12597cd49dfe1868a9d6a30
Vardaan <vardaansharma96@gmail.com>
2017-03-17 00:38:47 +0400

    asda

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
 <?php
 	echo "hello"
+
+$dsn = "pgsql:"
+    . "host=ec2-79-125-5-199.eu-west-1.compute.amazonaws.com;"
+    . "dbname=d3f5n2850gvvdk;"
+    . "user=dguqsrqlvkwikg;"
+    . "port=5432;"
+    . "sslmode=require;"
+    . "password=0df6d6a666c589ea364a2338859ec8ea352874effba3fdfcf8b1bbe69966ee20";
+
+$db = new PDO($dsn);
+echo "we"
+
 ?>
